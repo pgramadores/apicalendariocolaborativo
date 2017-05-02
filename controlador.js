909ceda7b64a27531c6f256e2347e85c2dbf5313
Juan Mora <raicerk@gmail.com>
2017-05-02 07:10:16 -0300

    Se agrega modelo, main y controlador, pendiente el desarrollo del controlador

---
+++ controlador.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('eventos');
+var config = require('./config');
+
+exports.AgregaEvento = function(req, res) {
+
+    try {
+
+            let Eventos = new eventos({
+                nombres       : req.body.nombres,
+                tituloevento  : req.body.tituloevento,
+                fechainicio   : req.body.fechainicio,
+                horainicio    : req.body.horainicio,
+                fechatermino  : req.body.fechatermino,
+                horatermino   : req.body.horatermino,
+                ubicacion     : req.body.ubicacion,
+                descripcion   : req.body.descripcion,
+                urlevento     : req.body.urlevento
+            });
+
+            //ingreso de usuario
+            var promise = Eventos.save();
+
+            promise.then(function (doc) {
+                return res.status(200).jsonp({ok:true});
+            })
+            .catch(function(err){
+
+                console.log(err);
+
+                return res.status(500).jsonp({ok:false});
+
+
+            });
+
+    }catch (e) {
+        console.log(e);
+        return res.status(500).jsonp({ok:false});
+    }
+
+};
---
+++ main.js
@@ -0,0 +1,84 @@
+// Importación de nuestro modelos
+require('./modelos/eventos');
+
+// Inyección de dependencias
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var helmet = require('helmet');
+var moment = require('moment');
+var frameguard = require('frameguard');
+var hsts = require('hsts');
+var fs = require('fs');
+var https = require('https');
+var ctrl = require('./controlador');
+var config = require('./config');
+
+var app = express();
+
+// Cargamos certificados de seguridad SSL/TSL
+// var options = {
+//    key  : fs.readFileSync(config.certificados.private),
+//    cert : fs.readFileSync(config.certificados.certificado)
+// };
+
+// Configuramos Express
+app.use(helmet());
+
+// Seguridad para no permitir el uso del sitio con iframe
+app.use(frameguard({ action: 'deny' }));
+
+// Seguridad para el uso exclusivo de protocolo https para la llamada a la api
+// Solicitar validación en sitio https://hstspreload.org
+app.use(hsts({
+    maxAge: 10886400,
+    includeSubDomains: true,
+    preload: true
+}))
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.set('port', config.puerto);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', config.domain);
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Content-Type','application/json');
+    next();
+});
+
+// Iniciamos las rutas de nuestro servidor/API
+var router = express.Router();
+
+// Ruta de bienvenida
+router.get('/', function(req, res) {
+   res.send({'Mensaje':'Bienvenido a la API REST Pro-Gramadores'});
+});
+
+// Rutas de registro
+router.post('/eventos/agregar', ctrl.AgregaEvento);
+
+// Ruta de consulta de eventos disponibles
+router.get('/eventos/consultar/',ctrl.ConsultaEventos);
+
+// Ruta de cancelación de suscripción
+router.post('/eventos/cancelar/:id',ctrl.CancelaEvento);
+
+// Ruta de aceptación de suscripción
+router.get('/eventos/consultar/:id',ctrl.ConsultaEvento);
+
+app.use(router);
+
+// Iniciamos el servidor y la base de datos
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/pgramadores', function(err) {
+    if (!err) {
+        https.createServer(options, app).listen(app.get('port'), function () {
+           console.log('Express corriendo en '+config.domain+':'+config.puerto);
+        });
+    }else{
+        console.log(err.message);
+    }
+});
