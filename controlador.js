16564b8489b9f1e812cc97e4d8b32982ba0e9da8
Juan Mora <raicerk@gmail.com>
2017-07-30 23:03:40 -0400

    Se deja funcional carga de eventos y consulta de los mismos

--- controlador.js
+++ controlador.js
@@ -1,47 +1,51 @@
 var mongoose = require('mongoose');
-var User = mongoose.model('eventos');
+var Eventos = mongoose.model('eventos');
 var config = require('./config');
 
 exports.AgregaEvento = function(req, res) {
 
-  try {
+	try {
 
-    let Eventos = new eventos({
-      nombres: req.body.nombres,
-      tituloevento: req.body.tituloevento,
-      fechainicio: req.body.fechainicio,
-      horainicio: req.body.horainicio,
-      fechatermino: req.body.fechatermino,
-      horatermino: req.body.horatermino,
-      ubicacion: req.body.ubicacion,
-      descripcion: req.body.descripcion,
-      urlevento: req.body.urlevento
-    });
+		let event = new Eventos({
+			title: req.body.title,
+			startTime: req.body.startTime,
+			endTime: req.body.endTime,
+			location: req.body.location,
+			description: req.body.description,
+			urlevent: req.body.urlevent,
+			state: false
+		});
 
-    //ingreso de usuario
-    var promise = Eventos.save();
+		//ingreso de evento
+		var promise = event.save();
 
-    promise.then(function(doc) {
-        return res.status(200).jsonp({
-          ok: true
-        });
-      })
-      .catch(function(err) {
+		promise.then(function(doc) {
+				return res.status(200).jsonp({
+					creado: true
+				});
+			})
+			.catch(function(err) {
 
-        console.log(err);
+				console.log(err);
 
-        return res.status(500).jsonp({
-          ok: false
-        });
-
-
-      });
-
-  } catch (e) {
-    console.log(e);
-    return res.status(500).jsonp({
-      ok: false
-    });
-  }
+				return res.status(500).jsonp({
+					creado: false
+				});
+			});
+	} catch (e) {
+		console.log(e);
+		return res.status(500).jsonp({
+			creado: false
+		});
+	}
 
 };
+
+exports.ConsultaEventos = function(req, res) {
+	Eventos.find(function(err, data) {
+		if (err) {
+			return res.send(500, err.message);
+		}
+		res.status(200).jsonp(data);
+	});
+}
--- modelos/eventos.js
+++ modelos/eventos.js
@@ -3,14 +3,13 @@ var mongoose = require('mongoose'),
   Schema   = mongoose.Schema;
 
 var Eventos = new Schema({
-  tituloevento: { type : String,  required: true },
-  fechainicio : { type : Date,    required : true },
-  horainicio  : { type : String,  required : true },
-  fechatermino: { type : Date,    required : true },
-  horatermino : { type : String,  required : true },
-  ubicacion   : { type : String,  required : true },
-  descripcion : { type : String,  required : true },
-  urlevento   : { type : String,  required : true }
+  title       : { type : String,  required : true },
+  startTime   : { type : Date,    required : true },
+  endTime     : { type : Date,    required : true },
+  location    : { type : String,  required : true },
+  description : { type : String,  required : true },
+  urlevent    : { type : String,  required : true },
+  state       : { type : Boolean, required : true }
 });
 
 module.exports = mongoose.model('eventos', Eventos);
